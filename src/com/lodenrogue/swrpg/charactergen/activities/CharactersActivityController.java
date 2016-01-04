909add0fd762f5f67e633f6a5333c8f02350535f
Miguel Hernandez <phyzix5761@gmail.com>
2016-01-04 07:16:36 -0500

    Added ListView to CharactersActivity

--- src/com/lodenrogue/swrpg/charactergen/activities/CharactersActivityController.java
+++ src/com/lodenrogue/swrpg/charactergen/activities/CharactersActivityController.java
@@ -9,12 +9,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 
 public class CharactersActivityController implements Initializable {
 	@FXML
-	private ScrollPane charactersPane;
+	private ListView<String> charactersList;
 	@FXML
 	private Button selectBtn;
 	@FXML
